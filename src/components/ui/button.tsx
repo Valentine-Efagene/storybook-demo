aee801a235a81e6fef6a732ba835952da443c55f
Valentine Efagene <efagenevalentine@gmail.com>
2025-11-06 11:41:05 +0100

    feat: add Breadcrumb and Empty components with respective subcomponents and styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Spinner } from "./spinner"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -64,34 +65,34 @@ function Button({
 }: ButtonProps) {
   const Comp = asChild ? Slot : "button"
 
-  const LoadingSpinner = () => (
-    <svg
-      className="animate-spin size-4"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        strokeWidth="4"
-      />
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      />
-    </svg>
-  )
+  // const LoadingSpinner = () => (
+  //   <svg
+  //     className="animate-spin size-4"
+  //     xmlns="http://www.w3.org/2000/svg"
+  //     fill="none"
+  //     viewBox="0 0 24 24"
+  //   >
+  //     <circle
+  //       className="opacity-25"
+  //       cx="12"
+  //       cy="12"
+  //       r="10"
+  //       stroke="currentColor"
+  //       strokeWidth="4"
+  //     />
+  //     <path
+  //       className="opacity-75"
+  //       fill="currentColor"
+  //       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+  //     />
+  //   </svg>
+  // )
 
   const renderContent = () => {
     if (loading) {
       return (
         <>
-          <LoadingSpinner />
+          <Spinner />
           {children && <span>Loading...</span>}
         </>
       )
