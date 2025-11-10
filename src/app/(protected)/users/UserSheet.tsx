38e002f23ec68b2ddf7acce8f982213710114562
Valentine Efagene <efagenevalentine@gmail.com>
2025-11-10 20:21:59 +0100

    feat: enhance UserSheet with DetailCard components for user information display; update StatusTag styles for consistency

--- src/app/(protected)/users/UserSheet.tsx
+++ src/app/(protected)/users/UserSheet.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import DetailCard from "@/components/DetailCard"
 import StatusTag from "@/components/StatusTag"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
@@ -15,7 +16,7 @@ import {
 import ImageHelper from "@/lib/helpers/ImageHelper"
 import UserHelper from "@/lib/helpers/UserHelper"
 import { User } from "@/types/user"
-import { Edit2, Play } from "lucide-react"
+import { Edit2, Play, X } from "lucide-react"
 import { useMemo } from "react"
 
 interface Props {
@@ -30,8 +31,18 @@ export function UserSheet({ open, setOpen, user }: Props) {
 
     return (
         <Sheet open={open} onOpenChange={setOpen}>
-            <SheetContent className="w-full sm:w-[560px] sm:max-w-[560px] p-0 gap-4">
-                <SheetTitle className="py-6 border-b">
+            <SheetContent className="w-full sm:w-[560px] sm:max-w-[560px] p-0 gap-4 [&>button]:hidden max-h-full overflow-auto">
+                <SheetTitle className="py-2 border-b px-6 flex items-center justify-between">
+                    <Button
+                        variant="ghost"
+                        size="sm"
+                        onClick={() => setOpen(false)}
+                        className="ml-auto"
+                        icon={<X className="h-4 w-4" />}
+                        iconPosition="left"
+                    >
+                        Close
+                    </Button>
                 </SheetTitle>
                 <SheetHeader>
                     <SheetDescription>
@@ -61,12 +72,37 @@ export function UserSheet({ open, setOpen, user }: Props) {
                     </SheetDescription>
                 </SheetHeader>
                 <Separator />
-                <div className="flex flex-col gap-4 px-6">
-                    <div className="flex justify-between">
-                        <span className="text-[var(--primary-text)] font-medium text-sm">Personal Information</span>
-                        <Button size='sm' icon={<Edit2 />} iconPosition="left" variant='subtle'>
-                            Edit
-                        </Button>
+                <div className="flex flex-col gap-[3rem]">
+                    <div className="flex flex-col gap-4 px-6">
+                        <div className="flex justify-between">
+                            <span className="text-[var(--primary-text)] font-medium text-sm">Personal Information</span>
+                            <Button size='sm' icon={<Edit2 />} iconPosition="left" variant='subtle'>
+                                Edit
+                            </Button>
+                        </div>
+                        <div className="rounded-xl border p-5 grid grid-cols-2 gap-5">
+                            <DetailCard label="First Name" value={user.first_name} />
+                            <DetailCard label="Last Name" value={user.last_name} />
+                            <DetailCard label="Email" type="email" nChars={20} value={user.email} />
+                            <DetailCard label="Phone Number" value={user.phone} />
+                            <DetailCard label="BVN" value={user.bvn} />
+                            <DetailCard label="Government Issues ID" nChars={20} value={user.identity_document} />
+                            <DetailCard label="Country of Residence" value={user.country} />
+                            <DetailCard label="City" value={user.address} />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 px-6">
+                        <div className="flex justify-between">
+                            <span className="text-[var(--primary-text)] font-medium text-sm">Employment Information</span>
+                            <Button size='sm' icon={<Edit2 />} iconPosition="left" variant='subtle'>
+                                Edit
+                            </Button>
+                        </div>
+                        <div className="rounded-xl border p-5 grid grid-cols-2 gap-5">
+                            <DetailCard label="Occupation" value={user.job_title} />
+                            <DetailCard label="Company Name" value={user.name_of_employer} />
+                            <DetailCard label="Salary Range" nChars={20} value={user.monthly_net_salary} />
+                        </div>
                     </div>
                 </div>
             </SheetContent>
--- src/components/StatusTag.tsx
+++ src/components/StatusTag.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function StatusTag({ dotClassName, statusText }: Props) {
     return (
-        <div className="flex flex-nowrap gap-2 items-center text-sm font-medium text-[var(--text-secondary)] border rounded-[100px] px-2 py-1 h-fit">
+        <div className="flex flex-nowrap gap-2 items-center text-sm font-medium text-[var(--secondary-text)] border rounded-[100px] px-2 py-1 h-fit">
             <div className={`w-2 h-2 rounded-full bg-[var(--tertiary-bg)] ${dotClassName ?? ''}`}></div>
             <div>{statusText ?? "Not Started"}</div>
         </div>
